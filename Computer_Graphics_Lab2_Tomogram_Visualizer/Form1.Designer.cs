8da438cd0accd334c09a45f1ea46fc63f29564a7
mefja <hippiman1221@gmail.com>
2020-04-14 19:35:02 +0300

    Начало работы

---
+++ Computer_Graphics_Lab2_Tomogram_Visualizer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Computer_Graphics_Lab2_Tomogram_Visualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(776, 456);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.GlControl1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(318, 577);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить Файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 504);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(776, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 621);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Computer_Graphics_Lab2_Tomogram_Visualizer/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+
+namespace Computer_Graphics_Lab2_Tomogram_Visualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bin bin=new Bin();
+        View view=new View();
+        bool loaded = false;
+        int currentLayer=0;
+        internal class Bin
+        {
+            public static int X, Y, Z;
+            public static short[] array;
+            public Bin() { }
+
+            public void readBin(string path)
+            {
+                if (File.Exists(path))
+                {
+                    BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+
+                    X = reader.ReadInt32();
+                    Y = reader.ReadInt32();
+                    Z = reader.ReadInt32();
+
+                    int arraySize = X * Y * Z;
+
+                    array = new short[arraySize];
+                    for (int i = 0; i < arraySize; i++)
+                    {
+                        array[i] = reader.ReadInt16();
+                    }
+                }
+            }
+        }
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value > max)
+                return max;
+            if (value < min)
+                return min;
+            return value;
+        }
+        internal class  View
+        {
+            public void SetupView(int width, int height)
+            {
+                GL.ShadeModel(ShadingModel.Smooth);
+                GL.MatrixMode(MatrixMode.Projection);
+                GL.LoadIdentity();
+                GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+                GL.Viewport(0, 0, width, height);
+
+            }
+            Color TransferFuncltion(short value)
+            {
+                int min = 0;
+                int max = 2000;
+                int newVal = Clamp((value - min) * 255 / (max - min), 0, 255);
+                return Color.FromArgb(255, newVal, newVal, newVal);
+            }
+
+            public void DrawQuads(int layerNumber)
+            {
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                GL.Begin(BeginMode.Quads);
+
+                for(int x_coord=0;x_coord<Bin.X-1;x_coord++)
+                {
+                    for(int y_coord=0;y_coord<Bin.Y-1;y_coord++)
+                    {
+                        short value;
+                        value = Bin.array[x_coord + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
+                        GL.Color3(TransferFuncltion(value));
+                        GL.Vertex2(x_coord, y_coord);
+
+                        value= Bin.array[x_coord + (y_coord+1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+                        GL.Color3(TransferFuncltion(value));
+                        GL.Vertex2(x_coord, y_coord+1);
+
+                        value = Bin.array[x_coord+1 + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+                        GL.Color3(TransferFuncltion(value));
+                        GL.Vertex2(x_coord+1, y_coord + 1);
+
+                        value = Bin.array[x_coord + 1 + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
+                        GL.Color3(TransferFuncltion(value));
+                        GL.Vertex2(x_coord + 1, y_coord);
+                    }
+                }
+                GL.End();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog()==DialogResult.OK)
+            {
+                string str = dialog.FileName;
+                bin.readBin(str);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                loaded = true;
+                glControl1.Invalidate();
+                trackBar1.Maximum = Bin.Z-1;
+            }
+        }
+
+        private void GlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if(loaded)
+            {
+                view.DrawQuads(currentLayer);
+                glControl1.SwapBuffers();
+            }
+        }
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            currentLayer = trackBar1.Value;
+            if (loaded)
+            {
+                view.DrawQuads(currentLayer);
+                glControl1.SwapBuffers();
+            }
+        }
+    }
+}
