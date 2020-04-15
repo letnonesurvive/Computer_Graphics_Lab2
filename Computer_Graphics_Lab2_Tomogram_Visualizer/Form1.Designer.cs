a4ace2f1ff2b016baeb43d1ddb3b16ab592087f2
mefja <hippiman1221@gmail.com>
2020-04-15 21:56:03 +0300

    Доделал, осталось практическое задание

--- Computer_Graphics_Lab2_Tomogram_Visualizer/Form1.Designer.cs
+++ Computer_Graphics_Lab2_Tomogram_Visualizer/Form1.Designer.cs
@@ -31,15 +31,17 @@
             this.glControl1 = new OpenTK.GLControl();
             this.button1 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // glControl1
             // 
             this.glControl1.BackColor = System.Drawing.Color.Black;
-            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Location = new System.Drawing.Point(12, 55);
             this.glControl1.Name = "glControl1";
-            this.glControl1.Size = new System.Drawing.Size(776, 456);
+            this.glControl1.Size = new System.Drawing.Size(919, 592);
             this.glControl1.TabIndex = 0;
             this.glControl1.VSync = false;
             this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.GlControl1_Paint);
@@ -47,7 +49,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(318, 577);
+            this.button1.Location = new System.Drawing.Point(422, 704);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(153, 32);
             this.button1.TabIndex = 1;
@@ -57,22 +59,49 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 504);
+            this.trackBar1.Location = new System.Drawing.Point(12, 653);
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(776, 45);
+            this.trackBar1.Size = new System.Drawing.Size(919, 45);
             this.trackBar1.TabIndex = 2;
             this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton1.Location = new System.Drawing.Point(21, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 24);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Quads";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton2.Location = new System.Drawing.Point(101, 22);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 24);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Texture";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 621);
+            this.ClientSize = new System.Drawing.Size(943, 748);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.glControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -84,6 +113,8 @@
         private OpenTK.GLControl glControl1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- Computer_Graphics_Lab2_Tomogram_Visualizer/Form1.cs
+++ Computer_Graphics_Lab2_Tomogram_Visualizer/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
 
 namespace Computer_Graphics_Lab2_Tomogram_Visualizer
 {
@@ -22,6 +23,12 @@ namespace Computer_Graphics_Lab2_Tomogram_Visualizer
         View view=new View();
         bool loaded = false;
         int currentLayer=0;
+
+        int FrameCount;
+        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+
+        bool needReload = false;
+
         internal class Bin
         {
             public static int X, Y, Z;
@@ -48,6 +55,18 @@ namespace Computer_Graphics_Lab2_Tomogram_Visualizer
                 }
             }
         }
+
+        void DisplayFPS()
+        {
+            if(DateTime.Now>=NextFPSUpdate)
+            {
+                this.Text = String.Format("CT Visualizer (fps={0})", FrameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                FrameCount = 0;
+            }
+            FrameCount++;
+        }
+
         public static int Clamp(int value, int min, int max)
         {
             if (value > max)
@@ -56,8 +75,116 @@ namespace Computer_Graphics_Lab2_Tomogram_Visualizer
                 return min;
             return value;
         }
-        internal class  View
+       
+        private void Button1_Click(object sender, EventArgs e)
         {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog()==DialogResult.OK)
+            {
+                string str = dialog.FileName;
+                bin.readBin(str);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                loaded = true;
+                glControl1.Invalidate();
+                trackBar1.Maximum = Bin.Z-1;
+            }
+        }
+
+        private void GlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if(loaded)
+            {
+                if (radioButton1.Checked)
+                    view.DrawQuads(currentLayer);
+                else
+                {
+                    if (needReload)
+                    {
+                        view.generateTextureImage(currentLayer);
+                        view.Load2DTexture();
+                        needReload = false;
+                    }
+                    view.DrawTexture();
+                }
+                glControl1.SwapBuffers();
+            }
+        }
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            currentLayer = trackBar1.Value;
+            needReload = true;
+        }
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while(glControl1.IsIdle)
+            {
+                DisplayFPS();
+                glControl1.Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
+        }
+        internal class View
+        {
+            Bitmap textureImage;
+            int VBOtexture;
+            public void Load2DTexture()
+            {
+                GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
+                BitmapData data = textureImage.LockBits(
+                    new Rectangle(0, 0, textureImage.Width, textureImage.Height),
+                    ImageLockMode.ReadOnly,
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                    data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                    PixelType.UnsignedByte, data.Scan0);
+
+                textureImage.UnlockBits(data);
+
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
+                    (int)TextureMinFilter.Linear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
+                    (int)TextureMagFilter.Linear);
+
+                ErrorCode Er = GL.GetError();
+                string str = Er.ToString();
+            }
+            public void generateTextureImage(int layerNumber)
+            {
+                textureImage = new Bitmap(Bin.X, Bin.Y);
+                for (int i = 0; i < Bin.X; i++)
+                {
+                    for (int j = 0; j < Bin.Y; j++)
+                    {
+                        int pixelNumber = i + j * Bin.X + layerNumber * Bin.X * Bin.Y;
+                        textureImage.SetPixel(i, j, TransferFuncltion(Bin.array[pixelNumber]));
+                    }
+                }
+            }
+            public void DrawTexture()
+            {
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                GL.Enable(EnableCap.Texture2D);
+                GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
+
+                GL.Begin(BeginMode.Quads);
+                GL.Color3(Color.White);
+                GL.TexCoord2(0f, 0f);
+                GL.Vertex2(0, 0);
+                GL.TexCoord2(0f, 1f);
+                GL.Vertex2(0, Bin.Y);
+                GL.TexCoord2(1f, 1f);
+                GL.Vertex2(Bin.X, Bin.Y);
+                GL.TexCoord2(1f, 0f);
+                GL.Vertex2(Bin.X, 0);
+                GL.End();
+
+                GL.Disable(EnableCap.Texture2D);
+            }
             public void SetupView(int width, int height)
             {
                 GL.ShadeModel(ShadingModel.Smooth);
@@ -80,22 +207,22 @@ namespace Computer_Graphics_Lab2_Tomogram_Visualizer
                 GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
                 GL.Begin(BeginMode.Quads);
 
-                for(int x_coord=0;x_coord<Bin.X-1;x_coord++)
+                for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
                 {
-                    for(int y_coord=0;y_coord<Bin.Y-1;y_coord++)
+                    for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
                     {
                         short value;
                         value = Bin.array[x_coord + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
                         GL.Color3(TransferFuncltion(value));
                         GL.Vertex2(x_coord, y_coord);
 
-                        value= Bin.array[x_coord + (y_coord+1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+                        value = Bin.array[x_coord + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
                         GL.Color3(TransferFuncltion(value));
-                        GL.Vertex2(x_coord, y_coord+1);
+                        GL.Vertex2(x_coord, y_coord + 1);
 
-                        value = Bin.array[x_coord+1 + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+                        value = Bin.array[x_coord + 1 + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
                         GL.Color3(TransferFuncltion(value));
-                        GL.Vertex2(x_coord+1, y_coord + 1);
+                        GL.Vertex2(x_coord + 1, y_coord + 1);
 
                         value = Bin.array[x_coord + 1 + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
                         GL.Color3(TransferFuncltion(value));
@@ -103,38 +230,6 @@ namespace Computer_Graphics_Lab2_Tomogram_Visualizer
                     }
                 }
                 GL.End();
-            }
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            if(dialog.ShowDialog()==DialogResult.OK)
-            {
-                string str = dialog.FileName;
-                bin.readBin(str);
-                view.SetupView(glControl1.Width, glControl1.Height);
-                loaded = true;
-                glControl1.Invalidate();
-                trackBar1.Maximum = Bin.Z-1;
-            }
-        }
-
-        private void GlControl1_Paint(object sender, PaintEventArgs e)
-        {
-            if(loaded)
-            {
-                view.DrawQuads(currentLayer);
-                glControl1.SwapBuffers();
-            }
-        }
-        private void TrackBar1_Scroll(object sender, EventArgs e)
-        {
-            currentLayer = trackBar1.Value;
-            if (loaded)
-            {
-                view.DrawQuads(currentLayer);
-                glControl1.SwapBuffers();
             }
         }
     }
